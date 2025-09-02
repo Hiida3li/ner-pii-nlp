839270547ff0f176da02d8c8c03671573950c7ea
Hewida Ali <112735381+Hiida3li@users.noreply.github.com>
2025-09-02 09:45:41 +0400

    pii detector main page redesign

--- src/static/js/privacy_chat.js
+++ src/static/js/privacy_chat.js
@@ -318,7 +318,7 @@ class PrivacyChat {
                         </div>
                     </div>
                 </div>
-                <h1 class="welcome-title">BOT.AI</h1>
+                <h1 class="welcome-title">Blot</h1>
                 <p class="welcome-subtitle">
                     Good to see you! How can I help?
                 </p>
